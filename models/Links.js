5d9e4a95ef0517dc4d0e585929555489752fcdaa
TCMooney <tcmooney88@gmail.com>
2020-09-03 23:04:46 -0400

    built schema for home page links

---
+++ models/Links.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const LinksSchema = new Schema({
+  title: {
+    type: String,
+    required: true
+  },
+  path: {
+    type: String,
+    required: true
+  },
+  imageUrl: {
+    type: String,
+    required: true
+  }
+})
+
+module.exports = Links = mongoose.model('links', LinksSchema);
